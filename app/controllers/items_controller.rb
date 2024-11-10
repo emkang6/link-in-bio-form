aac61e44e8e6f86f6498f9fc203ab8698bfd8dc7
emkang6 <hkang6@uchicago.edu>
2024-11-10 08:33:56 +0000

    initial commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,26 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def display_form
+
+    render({ :template => "item_templates/backdoor_form"})
+  end
+
+
+  def create_item
+
+    #Parameters: {"query_link_url"=>"1", "query_link_description"=>"2", "query_thumbnail_url"=>"3"}
+
+    i=Item.new
+    i.link_url = params.fetch("query_link_url")
+    i.link_description = params.fetch("query_link_description")
+    i.thumbnail_url = params.fetch("query_thumbnail_url")
+    i.save
+
+    # render({ :template => "item_templates/create"})
+    redirect_to("/")
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
+  get("/backdoor", { :controller => "items", :action => "display_form"})
+
+  post("/insert_item", { :controller => "items", :action => "create_item"})
+
   get("/", { :controller => "items", :action => "index" })
 end
